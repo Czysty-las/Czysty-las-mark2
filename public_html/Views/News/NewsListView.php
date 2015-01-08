33cdc3257d148fd711863b6a39ba26420871eb6d
ukidabek <ukidabek@gmail.com>
2015-01-08 21:36:06 +0100

    Dalsze prace nad Newsami.

--- public_html/Views/News/NewsListView.php
+++ public_html/Views/News/NewsListView.php
@@ -6,8 +6,21 @@
             <div class="newsList">
                 <div class="newsItem">
                     
+                    <div class="titleBar">
+                        <div class="title">Tytuł</div><div class="author">Ja</div><div class="date">20-12-2014</div>
+                    </div>
+                    
+                    <div class="content">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu malesuada dolor, nec fringilla ante. Donec pulvinar consectetur molestie. Vestibulum a justo a lorem tempor feugiat. Nullam neque tellus, molestie sit amet nunc vel, cursus scelerisque massa. Nam in nulla quis libero rutrum imperdiet. Duis leo augue, fermentum vel pulvinar vel, aliquam id sapien. Nullam dignissim condimentum aliquet. Nullam ante urna, facilisis et lorem vel, tristique iaculis justo. Vestibulum quis tempor eros, at blandit purus. Vivamus vehicula tellus diam, id maximus lacus vestibulum non. Suspendisse facilisis ornare odio vitae porta. Sed sed velit vel justo ultrices tincidunt sed et magna. Fusce ut ultrices arcu. Cras eu sem in velit vestibulum hendrerit eget porta elit. Vivamus lacus dui, dapibus ut iaculis vel, laoreet ut erat.   
+                    </div>
+                    
+                    <div class="editBar">
+                        <a href="index.php?action=edit_news&user=">Edytuj</a><a href="index.php?action=deleate_news&user=">Usuń</a>
+                    </div>
                 </div>
+                
             </div>
         </div>
-    </body>
+    </div>
+</body>
 </html>
