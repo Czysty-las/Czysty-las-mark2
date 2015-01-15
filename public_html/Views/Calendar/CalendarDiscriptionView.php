c4e8be385277e532a3c9a996553427639748e9e4
ZealotSE <sliski69@gmail.com>
2015-01-15 23:49:27 +0100

    Zmiany w kalendarium

--- public_html/Views/Calendar/CalendarDiscriptionView.php
+++ public_html/Views/Calendar/CalendarDiscriptionView.php
@@ -4,21 +4,19 @@
         <?php include _ROOT_PATH . DIRECTORY_SEPARATOR . 'Views' . DIRECTORY_SEPARATOR . 'ContentManagementSystemParts' . DIRECTORY_SEPARATOR . 'Label.php'; ?>
         <div class="calendarModule">              
             <form action="CMS.php" method="post">
-            <div class="calendarDescriptionTopic">
+            
+                    <input hidden="hidden" name="Id" value="<?php echo $event->Id;  ?>">
+                    <p>Topic: <input name="Topic" value="<?php echo $event->Topic;  ?>"/></p>
+                    <p>Description: <textarea name="login" value="<?php echo $event->Description;  ?>"></textarea></p>
+                    <p>Event date: <input type="date" name="Date" value="<?php echo $event->Date;  ?>"/></p>
+                    <p>Created by: <?php echo $event->UserLogin;  ?></p>              
                 
-                    <p>Topic: <input name="login" value="<?php echo $event->Topic;  ?>"/></p>
-                    <p>Description: <textarea> name="login" value="<?php echo $event->Description;  ?>"</textarea></p>
-                    <p>Event date: <input type="date" name="date" value="<?php echo $event->Date;  ?>"/></p>
-                    <p>Created by: <?php echo $event->UserLogin;  ?></p>
-                    
-                
-
             </div>
             <div class="calendarDescriptionButtons">    
             <a class="formButton" href="CMS.php?action=list_calendar">Wróć</a>
-            <a class="formButton" href="CMS.php?action=delete_calendar">Usuń</a>
+            <a class="formButton" href="CMS.php?action=delete_calendar">Usuń</a>*/
             <p><button type="submit" name="function" value="edit_calendar">3c======3</button></p>
-            </div>
+            
             </form>
 
             
